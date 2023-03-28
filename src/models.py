d6d650d9463fb624a4871917a30ef2bab47ac376
jpablop123 <juanpablopinto123@icloud.com>
2023-03-28 18:02:12 +0000

    listo

--- src/models.py
+++ src/models.py
@@ -8,26 +8,28 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class People(Base):
+    id = Column(Integer, primary_key=True)
+    __tablename__ = 'people'
+    name = Column(String(250), nullable=False)
+    gender = Column(String(10), nullable=False)
+
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Favorite(Base):
+    __tablename__ ='favorite'
+    id = Column(Integer, primary_key=True)
+    people_id = Column(Integer, ForeignKey("people.id"))
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+    vehicle_id = Column(Integer, ForeignKey("vehicle.id"))
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
